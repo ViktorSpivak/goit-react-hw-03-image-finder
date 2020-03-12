c5a85bce193193c591913bfcf16622eaa4bee4ee
Viktor Spivak <spivakmailbox@gmail.com>
2020-03-12 17:59:37 +0200

    Updates

---
+++ precache-manifest.84c7b837a0a92acb79df5266b4b827ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8824acc9526e7ac7e566a98dfefcf2",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "9ce55468efbc34206d30",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.55d24024.chunk.css"
+  },
+  {
+    "revision": "220cb5d95133123c43c3",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3fe7aa07.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3fe7aa07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce55468efbc34206d30",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.78383611.chunk.js"
+  },
+  {
+    "revision": "4992c48e99877600891f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.ad90512f.js"
+  }
+]);
