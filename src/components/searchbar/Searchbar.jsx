544986c560df30bf0f047fbafb7b740e18192c7e
Viktor Spivak <spivakmailbox@gmail.com>
2020-03-18 22:13:25 +0200

    second commit

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -8,32 +8,24 @@ export class Searchbar extends Component {
 
   handleChange = ev => {
     this.setState({ [ev.target.name]: ev.target.value });
-
-    (ev.keyCode === 13 || ev.target.name === "find") &&
-      this.props.onSubmit(this.state.tag);
+  };
+  handleSubmit = ev => {
+    ev.preventDefault();
+    this.props.onSubmit(this.state.tag);
   };
   render() {
     const { tag } = this.state;
     return (
       <div className={style.Searchbar}>
-        <form className={style.SearchForm} form="search">
-          <button
-            name="find"
-            form="search"
-            type="submit"
-            value={this.state.tag}
-            className={style.SearchForm_button}
-            onClick={this.handleChange}
-          >
+        <form className={style.SearchForm} onSubmit={this.handleSubmit}>
+          <button name="find" type="submit" className={style.SearchForm_button}>
             <span className={style.SearchForm_button_label}></span>
           </button>
 
           <input
-            form="search"
             value={tag}
             name="tag"
             onChange={this.handleChange}
-            onKeyDown={this.handleChange}
             className={style.SearchForm_input}
             type="text"
             autoComplete="off"
